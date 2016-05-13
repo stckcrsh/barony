c8f24e3f4873480ad319dbda44b38838284c110b
Woolever, Zachary <zwoolev@transunion.com>
2016-05-13 10:31:09 -0500

    Integrate uicoe-toolbox # Updates * UIC-21 - build task to copy toolbox files to build folder * UIC-22 - inject toolbox css into the index.html * UIC-20 - see UIC-21 * UIC-23 - create the clean:build task to clean out the build folder * UIC-27 - created the task to copy the vendor files into the build folder also added them into the injector for index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,9 @@
 module.exports = function(grunt) {
 
-  var util = require('util');
-
   var userConfig = require('./build.config.js')();
   var taskConfig = {
     pkg: grunt.file.readJSON('package.json')
   };
-
-  // Creates the basic default which is build active
-  // allows us to just type grunt from the command line
-  grunt.registerTask("default", ["build-watch"]);
-
-  // Creates a runnable non minified application in the root build directory
-  grunt.registerTask('build', function() {});
-
-  grunt.registerTask('build-watch', function(){});
 
 
   grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
--- build.config.js
+++ build.config.js
@@ -3,26 +3,37 @@
 /**
  * This file/module contains all configuration for the build process.
  */
-module.exports = function () {
+module.exports = function() {
 
-  return {
-    // Main grunt operating directories
+	return {
+		// Main grunt operating directories
+		build_dir: 'build/',
+		build_public: '<%= build_dir %>public/',
+		src_dir: 'src/',
+		bower_components: 'bower_components/',
 
-    // Source locations for JS, LESS, ASSETS
-    app_files: {
-    },
-    vendor_files: {
-      js: [ // entries here cannot use ! for negativity since these are directly entered in the html
-        'node_modules/angular2/bundles/angular2-polyfills.js',
-        'node_modules/systemjs/dist/system.src.js',
-        'node_modules/rxjs/bundles/Rx.js',
-        'node_modules/angular2/bundles/angular2.dev.js',
-        'node_modules/angular2/bundles/http.dev.js'
-      ]
-    }
+		// Source locations for JS, LESS, ASSETS
+		
+		vendor_files: {
+			js: [ // entries here cannot use ! for negativity since these are directly entered in the html
+				'node_modules/angular2/bundles/angular2-polyfills.js',
+				'node_modules/systemjs/dist/system.src.js',
+				'node_modules/rxjs/bundles/Rx.js',
+				'node_modules/angular2/bundles/angular2.dev.js',
+				'node_modules/angular2/bundles/http.dev.js'
+			]
+		},
+		ui_toolbox: {
+			all: '<%= bower_components %>/UICoE-Toolbox/dist/**/*',
+			css: [
+				'bower_components/UICoE-Toolbox/dist/css/tu-bootstrap.min.css',
+				'bower_components/UICoE-Toolbox/dist/css/tu-main.min.css',
+				'bower_components/UICoE-Toolbox/dist/css/tu-mainGray.min.css'
+			]
+		}
 
 
-  };
+	};
 
 
 

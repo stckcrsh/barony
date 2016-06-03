f945bcaebe532952bd7fbed4598c5d005821e911
Shruti gubbi venkatasubbaiah <sgubbiv@transunion.com>
2016-06-03 14:47:54 -0500

    feauture/UIC-16 - user component

---
+++ src/app/users/user.smallDetail.component.ts
@@ -0,0 +1,57 @@
+import { Component ,OnInit , Input} from '@angular/core';
+import { UserService, User} from './shared/index';
+
+
+@Component({
+	selector: 'user-small-detail',
+	templateUrl: 'app/users/user.smallDetail.html',
+	providers: [UserService]
+})
+
+export class UserSmallDetailComponent implements OnInit  {
+	@Input('user-id') userId: number;
+	private user: User[];
+	private userSelected: boolean = false;
+	private isEditable: boolean = false;
+
+
+	constructor(private userService: UserService) {
+
+	}
+
+	ngOnInit(){
+		this.userSelected = true;
+		this.userService.getUserDetail(this.userId).subscribe(	user => this.user = user, error => console.log(error));
+    }
+
+    
+    editUserDetails(user:User[],event:any){
+    	if (this.isEditable) {
+    		if(event.target.nodeName =="INPUT"){
+    		 	this.isEditable = true;
+    		}
+    		else{
+    		 	this.isEditable = false;
+    		 	this.saveUserDetails(user);	
+    		}   		 
+    		
+    	}
+    	else{    		
+    		this.isEditable=true;
+    	}
+    }
+    
+    inputFocused(event:any){
+    	event.preventDefault();
+
+    }
+
+    saveUserDetails(user:User[]){
+    		//this.userService.createUser(this.userId,this.user)
+			   	
+    	
+    }
+
+
+
+}

37f6516646e73d4912cb82a9f53f3a943e00b287
Woolever, Zachary <zwoolev@transunion.com>
2016-06-06 13:52:36 -0500

    Smart vs Dumb Comments component ## Updates * created the comments.container smart component * updated the comments list/create to be dumb components * created a testbench as an example object to test a dumb component

--- src/app/comments/comments.list.component.ts
+++ src/app/comments/comments.list.component.ts
@@ -1,29 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { Component, Input} from '@angular/core';
 
-import { CommentsService, Comment } from './shared/index';
-import { CreateComment } from './comments.create.component';
+import { Comment } from './shared/index';
 
 @Component({
-	directives: [CreateComment],
-	providers: [CommentsService],
 	selector: 'sa-comments-list',
 	templateUrl: 'app/comments/comments.list.component.html'
 })
-export class CommentsList implements OnInit {
-	@Input('post-id')
-	public postId: number;
-	public comments$: Observable<Array<Comment>>;
-
-	constructor(public commentsService: CommentsService) {}
-
-	/**
-	 * Runs after the component has been initialized. Gets all the comments attached to a post
-	 */
-	public ngOnInit() {
-		this.comments$ = this.commentsService.comments$.map(res => res.filter(comment => comment.postId === this.postId));
-
-		this.commentsService.getComments();
-	}
+export class CommentsList {
+	@Input('comments')
+	public comments: Comment[];
 }
--- src/app/starter-app.component.ts
+++ src/app/starter-app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 import { Header, Footer } from './tu-components/index';
-import { CommentsList } from './comments/index';
+import { CommentsContainer } from './comments/index';
 
 @Component({
-	directives: [Header, Footer, CommentsList],
+	directives: [Header, Footer, CommentsContainer],
 	selector: 'tu-starter-app',
 	template: `
 	<tu-header></tu-header>
-	<sa-comments-list></sa-comments-list>
+	<sa-comments-container post-id="1" ></sa-comments-container>
 	<tu-footer></tu-footer>
 	`
 })

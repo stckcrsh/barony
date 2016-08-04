318b4f5884dc3c8721ee4b00838d4c62c4815348
Woolever, Zachary <zwoolev@transunion.com>
2016-08-04 16:23:13 -0500

    Toolbox watcher * update to allow watching for toolbox changes (mainly for ui developers) * updated some syntax issues * updated the tslint that was causing some issues

--- config/gulp/tasks/copy-toolbox.js
+++ config/gulp/tasks/copy-toolbox.js
@@ -12,4 +12,8 @@ if (envConfig.ENV === envConfig.ENVS.DEV) {
 		gulp.src(config.toolbox.favicons)
 			.pipe(copy(config.src, { prefix: 4 }));
 	});
+
+	gulp.task('watch-toolbox', function() {
+		return gulp.watch(config.toolbox.src, ['copy']);
+	});
 }

14fafc7f1bd014387ad08c5bcdca7e5701a4b3f6
Woolever, Zachary <zwoolev@transunion.com>
2016-08-26 10:43:45 -0500

    Lint errors for build process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule, provide } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { EffectsModule, mergeEffects } from '@ngrx/effects';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { routing, appRoutingProviders, appRoutingDeclarations } from './app.routes';
